29afe46f8d2daaa16a63c648c3c8ba1b4792fdec
Allie321 <allisonroberson04@gmail.com>
2018-07-27 15:32:19 +0000

    automatically backed up by learn

--- classpractice.rb
+++ classpractice.rb
@@ -14,11 +14,11 @@ class FacebookProfiles
   end
   
   attr_accessor :job 
-  
+  puts "my name is #{name} i am #{age} years old!"
 end
 
  allison FacebookProfile.new( allison,15)
  allison.bio = "this is my bio!"
- allison.bio = "something in the medical field"
+ allison.job = "something in the medical field"
  
- puts 
+ puts "i am a #{allison.job}""
